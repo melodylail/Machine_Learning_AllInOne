3700dde91fbbfb372e58f8aaba0213fcd1d71a5f
luq <qiang.lu@intel.com>
2018-02-08 02:17:29 +0800

    adding more resources

--- TensorFlow_pratice_book/3_2_HelloWorld.py
+++ TensorFlow_pratice_book/3_2_HelloWorld.py
@@ -22,17 +22,17 @@ print(mnist.validation.images.shape, mnist.validation.labels.shape)
 
 import tensorflow as tf
 sess = tf.InteractiveSession()
-x = tf.placeholder(tf.float32, [None, 784])
+x = tf.placeholder(tf.float32, [tf.int8, 784])
 
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-y_ = tf.placeholder(tf.float32, [None, 10])
+y_ = tf.placeholder(tf.float32, [tf.int8, 10])
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.GradientDescentOptimizer(0.8).minimize(cross_entropy)
 
 tf.global_variables_initializer().run()
 
